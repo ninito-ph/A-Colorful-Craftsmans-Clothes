2cbda742db649944f04511e4da42d0511308563a
Paulo Oliveira <paulo@ninito.me>
2022-01-15 17:17:55 -0300

    Update HarvestableAttributes.cs
    - Fix incorrect CreateMenus reference on CreateAssetMenu attribute
    - Add various properties, like name, sprite, respawn time and harvest effect

--- Assets/Game/Scripts/Runtime/Data/Attributes/HarvestableAttributes.cs
+++ Assets/Game/Scripts/Runtime/Data/Attributes/HarvestableAttributes.cs
@@ -2,9 +2,38 @@
 
 namespace Game.Editor.Data.Attributes
 {
+    /// <summary>
+    /// A class that contains the attributes of a harvestable
+    /// </summary>
     [CreateAssetMenu(fileName = CreateMenus.HARVESTABLE_ATTRIBUTES_FILENAME,
-        menuName = CreateMenus.GAME_MANAGER_PROXY_MENUNAME, order = CreateMenus.HARVESTABLE_ATTRIBUTES_ORDER)]
-    public class HarvestableAttributes : ScriptableObject
+        menuName = CreateMenus.HARVESTABLE_ATTRIBUTES_MENUNAME, order = CreateMenus.HARVESTABLE_ATTRIBUTES_ORDER)]
+    public class HarvestableAttributes : Attributes
     {
+        #region Private Fields
+
+        [Header("Harvestable Properties")]
+        [SerializeField]
+        private string harvestableName;
+
+        [SerializeField]
+        private Sprite harvestableSprite;
+
+        [SerializeField]
+        private float respawnTime;
+
+        [Header("Visual Effects")]
+        [SerializeField]
+        private GameObject harvestEffect;
+
+        #endregion
+
+        #region Properties
+
+        public string HarvestableName => harvestableName;
+        public Sprite HarvestableSprite => harvestableSprite;
+        public float RespawnTime => respawnTime;
+        public GameObject HarvestEffect => harvestEffect;
+
+        #endregion
     }
 }
