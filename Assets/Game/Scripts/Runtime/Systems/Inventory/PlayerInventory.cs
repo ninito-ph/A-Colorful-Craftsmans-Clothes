3462033031c17984dd69f756c8b3f6b09eade410
Paulo Oliveira <paulo@ninito.me>
2022-01-15 19:05:00 -0300

    Add basic PlayerInventory

---
+++ Assets/Game/Scripts/Runtime/Systems/Inventory/PlayerInventory.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Game.Runtime.Systems.Save;
+using Ninito.UsualSuspects;
+using UnityEngine;
+
+namespace Game.Runtime.Systems.Inventory
+{
+    /// <summary>
+    /// A class that manages the player's inventory.
+    /// </summary>
+    public sealed class PlayerInventory : MonoBehaviour
+    {
+        #region Private Fields
+
+        [SerializeField]
+        private ItemRegistry itemRegistry;
+        
+        private SerializedDictionary<IItem, int> _itemsByQuantity;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// IItem argument represents the last modified item, int represents the quantity of the item.
+        /// </summary>
+        public Action<IItem, int> OnInventoryModified { get; set; }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Adds an item to the inventory.
+        /// </summary>
+        /// <param name="item">The item to add</param>
+        /// <param name="quantity">The quantity to add</param>
+        /// <exception cref="ArgumentOutOfRangeException">Exception thrown if trying to add a negative quantity</exception>
+        public void AddItem(IItem item, int quantity = 1)
+        {
+            if (quantity < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(quantity), quantity,
+                    "Quantity must be greater than or equal to zero.");
+            }
+
+            if (_itemsByQuantity.ContainsKey(item))
+            {
+                _itemsByQuantity[item] += quantity;
+            }
+            else
+            {
+                _itemsByQuantity.Add(item, quantity);
+            }
+        }
+
+        /// <summary>
+        /// Attempts to remove a given quantity of an item from the inventory.
+        /// </summary>
+        /// <param name="item">The item to remove</param>
+        /// <param name="quantity">The quantity to try to remove</param>
+        /// <returns>Whether the specified quantity of the given item was removed</returns>
+        public bool TryRemoveItem(IItem item, int quantity = 1)
+        {
+            if (!_itemsByQuantity.ContainsKey(item)) return false;
+            if (_itemsByQuantity[item] < quantity) return false;
+
+            _itemsByQuantity[item] -= quantity;
+
+            if (_itemsByQuantity[item] == 0)
+            {
+                _itemsByQuantity.Remove(item);
+            }
+
+            return true;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Saves the inventory
+        /// </summary>
+        private void SaveInventory()
+        {
+            // TODO: The inventory should only provide the inventory save class, refactor this later
+            List<int> itemHashes = _itemsByQuantity.Select(pair => pair.Key.GetHashCode()).ToList();
+            List<int> itemQuantities = _itemsByQuantity.Select(pair => pair.Value).ToList();
+
+            GameSave save = new GameSave(itemHashes, itemQuantities);
+            PlayerPrefs.SetString("InventorySave", JsonUtility.ToJson(save));
+        }
+
+        /// <summary>
+        /// Loads the inventory
+        /// </summary>
+        private void LoadInventory()
+        {
+            if (!PlayerPrefs.HasKey("InventorySave")) return;
+
+            GameSave save = JsonUtility.FromJson<GameSave>(PlayerPrefs.GetString("InventorySave"));
+            _itemsByQuantity = new SerializedDictionary<IItem, int>();
+
+            for (int index = 0; index < save.InventoryItemQuantities.Count; index++)
+            {
+                _itemsByQuantity.Add(itemRegistry.GetItemByHash(save.InventoryItemHashes[index]),
+                    save.InventoryItemQuantities[index]);
+            }
+        }
+
+        #endregion
+    }
+}
