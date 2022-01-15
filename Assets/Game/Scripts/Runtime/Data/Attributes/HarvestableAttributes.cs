766d797e9b9c00bc08425b8f3582c2a135053493
Paulo Oliveira <paulo@ninito.me>
2022-01-15 16:38:20 -0300

    Add HarvestableAttributes SO

---
+++ Assets/Game/Scripts/Runtime/Data/Attributes/HarvestableAttributes.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Game.Editor.Data.Attributes
+{
+    [CreateAssetMenu(fileName = CreateMenus.HARVESTABLE_ATTRIBUTES_FILENAME,
+        menuName = CreateMenus.GAME_MANAGER_PROXY_MENUNAME, order = CreateMenus.HARVESTABLE_ATTRIBUTES_ORDER)]
+    public class HarvestableAttributes : ScriptableObject
+    {
+    }
+}
