26c507ec880ea65c126f745d028dd4db564ee761
Paulo Oliveira <paulo@ninito.me>
2022-01-15 16:34:34 -0300

    Add base generic Attributes class

---
+++ Assets/Game/Scripts/Runtime/Data/Attributes/Attributes.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Game.Editor.Data.Attributes
+{
+    /// <summary>
+    /// A base class that serves as a base for any attribute collection of any entity 
+    /// </summary>
+    public abstract class Attributes
+    {
+        [TextArea]
+        public string Description;
+    }
+}
