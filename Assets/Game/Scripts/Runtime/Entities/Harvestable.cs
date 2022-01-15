057de5cfa756ec0717d6243feb152902ad7b8395
Paulo Oliveira <paulo@ninito.me>
2022-01-15 16:38:31 -0300

    Add Harvestable interactable

---
+++ Assets/Game/Scripts/Runtime/Entities/Harvestable.cs
@@ -0,0 +1,22 @@
+﻿using Ninito.UsualSuspects.Interactable;
+using UnityEngine;
+
+namespace Game.Runtime.Entities
+{
+    /// <summary>
+    /// A class that controls a harvestable object
+    /// </summary>
+    public sealed class Harvestable : MonoBehaviour, IInteractable
+    {
+        #region IInteractable Implementation
+
+        public string InteractionToolTip => "Flower";
+        
+        public void InteractWithAs(IInteractor interactor)
+        {
+            Destroy(gameObject);
+        }
+
+        #endregion
+    }
+}
