bfee39df4e7a722c2fe62ac1569d908e3afc6de7
Paulo Oliveira <paulo@ninito.me>
2022-01-15 19:03:27 -0300

    Add basic GameSave class

---
+++ Assets/Game/Scripts/Runtime/Systems/Save/GameSave.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Runtime.Systems.Save
+{
+    /// <summary>
+    /// A class that represents a save file.
+    /// </summary>
+    [Serializable]
+    public class GameSave
+    {
+        // TODO: make these two lists a single class
+        [SerializeField]
+        public List<int> InventoryItemHashes;
+        [SerializeField]
+        public List<int> InventoryItemQuantities;
+
+        public GameSave(List<int> inventoryItemHashes, List<int> inventoryItemQuantities)
+        {
+            InventoryItemHashes = inventoryItemHashes;
+            InventoryItemQuantities = inventoryItemQuantities;
+        }
+    }
+}
