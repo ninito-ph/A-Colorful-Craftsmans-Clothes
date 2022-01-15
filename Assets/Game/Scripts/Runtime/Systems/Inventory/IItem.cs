a4a8e04287a5657419045e73d9da13c4f3892be3
Paulo Oliveira <paulo@ninito.me>
2022-01-15 19:03:07 -0300

    Add IItem interface

---
+++ Assets/Game/Scripts/Runtime/Systems/Inventory/IItem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Game.Runtime.Systems.Inventory
+{
+    /// <summary>
+    /// An interface that describes something that can be owned as an item
+    /// </summary>
+    public interface IItem
+    {
+        #region Public Fields
+
+        public string Name { get; }
+        public string Description { get; }
+        public Sprite Icon { get; }
+
+        #endregion
+    }
+}
