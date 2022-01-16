f5b3016e3f0bc69ef63f6790f3c94bc1efda824c
Paulo Oliveira <paulo@ninito.me>
2022-01-16 18:13:02 -0300

    Add SimpleInteractor subclass capable of using TooltipDisplay

---
+++ Assets/Game/Scripts/Runtime/Systems/Interaction/Advanced2DInteractor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Ninito.UsualSuspects.Interactable;
+using UnityEngine;
+
+namespace Game.Runtime.Systems
+{
+    public sealed class Advanced2DInteractor : Simple2DInteractor
+    {
+        #region Private Fields
+
+        [Header("Dependencies")]
+        [SerializeField]
+        private TooltipDisplay tooltipDisplay;
+
+        #endregion
+
+        #region Protected Methods
+
+        protected override void UpdateInteractionTooltip()
+        {
+            IInteractable interactable = GetInteractableInVolume();
+            tooltipDisplay.SetTooltip(interactable == null ? String.Empty : interactable.InteractionToolTip);
+        }
+
+        #endregion
+    }
+}
