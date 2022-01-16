894ce2cef731248ebc96b7e74afadffec00f10a0
Paulo Oliveira <paulo@ninito.me>
2022-01-16 18:14:47 -0300

    Add DressableEntity class containing two ClothingSlots

---
+++ Assets/Game/Scripts/Runtime/Systems/Clothing/DressableEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Runtime.Systems.Clothing
+{
+    /// <summary>
+    /// A component that can be used to dress an entity
+    /// </summary>
+    public sealed class DressableEntity : MonoBehaviour
+    {
+        #region Private Fields
+
+        [Header("Slots")]
+        [SerializeField]
+        private ClothingSlot accessorySlot = new ClothingSlot();
+
+        [SerializeField]
+        private ClothingSlot bodySlot = new ClothingSlot();
+
+        #endregion
+    }
+}
