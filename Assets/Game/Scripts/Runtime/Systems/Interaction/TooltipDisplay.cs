682197e3c64e3e7ec2eeefbbf865820a1f75e16b
Paulo Oliveira <paulo@ninito.me>
2022-01-16 18:12:30 -0300

    Add TooltipDisplay class for better tooltips

---
+++ Assets/Game/Scripts/Runtime/Systems/Interaction/TooltipDisplay.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Game.Runtime.Systems
+{
+    /// <summary>
+    /// A class that controls a display for tooltips
+    /// </summary>
+    public sealed class TooltipDisplay : MonoBehaviour
+    {
+        #region Private Fields
+
+        [Header("Dependencies")]
+        [SerializeField]
+        private GameObject displayParent;
+
+        [SerializeField]
+        private TMP_Text tooltipText;
+
+        #endregion
+
+        #region Unity Callbacks
+
+        private void Reset()
+        {
+            displayParent = gameObject;
+        }
+
+        #endregion
+        
+        #region Public Methods
+
+        /// <summary>
+        /// Sets the text of the tooltip, and automatically disables the display if no tooltip is set
+        /// </summary>
+        /// <param name="tooltip">The tooltip to display</param>
+        public void SetTooltip(string tooltip)
+        {
+            if (String.IsNullOrEmpty(tooltip))
+            {
+                displayParent.SetActive(false);
+                tooltipText.text = String.Empty;
+            }
+            else
+            {
+                displayParent.SetActive(true);
+                tooltipText.text = tooltip;
+            }
+        }
+
+        #endregion
+    }
+}
