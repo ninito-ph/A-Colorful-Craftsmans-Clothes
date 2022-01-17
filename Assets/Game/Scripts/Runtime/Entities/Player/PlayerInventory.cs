f55428cd546acae98f738ec6a957c2e0679f55a4
Paulo Oliveira <paulo@ninito.me>
2022-01-17 13:16:40 -0300

    Create wrapper PlayerInventory class for ItemInventory class

---
+++ Assets/Game/Scripts/Runtime/Entities/Player/PlayerInventory.cs
@@ -0,0 +1,44 @@
+﻿using Game.Runtime.Systems.Inventory;
+using UnityEngine;
+
+namespace Game.Runtime.Entities.Player
+{
+    /// <summary>
+    /// A class that controls the player's inventory
+    /// </summary>
+    public sealed class PlayerInventory : MonoBehaviour
+    {
+        #region Private Fields
+
+        [Header("Configuration")]
+        [SerializeField]
+        private bool saveAndLoad = true;
+
+        [SerializeField]
+        private ItemInventory _contents = new ItemInventory();
+
+        #endregion
+
+        #region Properties
+
+        public ItemInventory Contents => _contents;
+
+        #endregion
+
+        #region Unity Callbacks
+
+        private void Start()
+        {
+            if (!saveAndLoad) return;
+            Contents.LoadInventory();
+        }
+
+        private void OnDestroy()
+        {
+            if (!saveAndLoad) return;
+            Contents.SaveInventory();
+        }
+
+        #endregion
+    }
+}
