c567049e8a321da4d809a47dd6c57688ba0bacee
Paulo Oliveira <paulo@ninito.me>
2022-01-17 16:22:34 -0300

    Add simple PlayerPrefs utility class to clear PlayerPrefs keys

---
+++ Assets/Game/Scripts/Editor/Utilities/PlayerPrefsUtility.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Game.Editor.Utilities
+{
+    /// <summary>
+    /// A class that provides simple utilities for working with PlayerPrefs.
+    /// </summary>
+    public static class PlayerPrefsUtility
+    {
+        /// <summary>
+        /// Clears out all PlayerPrefs keys
+        /// </summary>
+        [MenuItem("Tools/PlayerPrefs/Delete All")]
+        public static void ClearAll()
+        {
+            PlayerPrefs.DeleteAll();
+        }
+    }
+}
