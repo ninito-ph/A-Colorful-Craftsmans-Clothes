eea47defe2ede0c49345e53e860623d8a87e161d
Paulo Oliveira <paulo@ninito.me>
2022-01-17 16:30:28 -0300

    Add GetItemByID to ItemRegistry

--- Assets/Game/Scripts/Runtime/Data/Registries/ItemRegistry.cs
+++ Assets/Game/Scripts/Runtime/Data/Registries/ItemRegistry.cs
@@ -11,35 +11,37 @@ namespace Game.Runtime.Data.Registries
         menuName = CreateMenus.ITEM_REGISTRY_MENUNAME, order = CreateMenus.ITEM_REGISTRY_ORDER)]
     public sealed class ItemRegistry : Registry<ItemAttributes>
     {
-        #region Public Methods
-
-        public ItemAttributes GetItemByHash(int itemHash)
-        {
-            return Items.FirstOrDefault(item => item.GetHashCode() == itemHash);
-        }
-
-        #endregion
-
-        #region Public Methods
-
-        /// <summary>
-        /// Returns all items of the specified type
-        /// </summary>
-        /// <typeparam name="T">The desired type</typeparam>
-        /// <returns>An array of items of the desired type</returns>
-        public T[] GetAllItemsOfType<T>()
-        {
-            return Items.Where(item => item is T).Cast<T>().ToArray();
-        }
-
-        #endregion
-        
         #region Private Methods
 
         // Normally I wouldn't need to do this, but ContextMenu not showing up in inherited classes
         // is a regression bug present in this Unity version :(
         [ContextMenu("Auto fill")]
         private void FillSelf() => GetAllProjectItems();
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Finds an item by its ID
+        /// </summary>
+        /// <param name="itemID">The ID of the searched item</param>
+        /// <returns>An item with a specified ID</returns>
+        public ItemAttributes GetItemByID(int itemID)
+        {
+            return Items.FirstOrDefault(item => item.ID == itemID);
+        }
+        
+        /// <summary>
+        /// Adds an item to the registry and sets its ID.
+        /// </summary>
+        /// <param name="item">The item to add</param>
+        public override void Add(ItemAttributes item)
+        {
+            if (Items.Contains(item)) return;
+            Items.Add(item);
+            item.ID = Items.Count - 1;
+        }
 
         #endregion
     }
