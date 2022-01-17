5d9bb3746ca0489734ac5bbaa26f74564aa47b97
Paulo Oliveira <paulo@ninito.me>
2022-01-17 09:58:59 -0300

    Add PauseMenu class

---
+++ Assets/Game/Scripts/Runtime/UI/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using Game.Runtime.Systems;
+using Ninito.UnityExtended.WindowManager;
+using UnityEngine;
+
+namespace Game.Runtime.UI
+{
+    /// <summary>
+    /// A class that controls the in-game pause menu
+    /// </summary>
+    public sealed class PauseMenu : MonoBehaviour
+    {
+        #region Private Fields
+
+        [Header("Dependencies")]
+        [SerializeField]
+        private WindowManager windowManager;
+
+        [Header("UI Keys")]
+        [SerializeField]
+        private string hudKey = "HUD";
+
+        #endregion
+        
+        #region Unity Callbacks
+
+        private void OnEnable()
+        {
+            TimeManager.Pause();
+        }
+
+        private void OnDisable()
+        {
+            TimeManager.Resume();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                windowManager.SwitchToMenu(hudKey);
+            }
+        }
+
+        #endregion
+    }
+}
