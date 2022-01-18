3fa9f026082e1b22137bdb5596aac2bbaa39de41
Paulo Oliveira <paulo@ninito.me>
2022-01-17 22:19:55 -0300

    Add simple script which attaches a transform to the camera

---
+++ Assets/Game/Scripts/Runtime/Utility/CameraAttacher.cs
@@ -0,0 +1,36 @@
+﻿using Game.Runtime.Core;
+using Ninito.TheUsualSuspects.Utilities;
+using UnityEngine;
+
+namespace Game.Runtime.Utility
+{
+    /// <summary>
+    /// A script that attaches its parent transform to the player
+    /// </summary>
+    public sealed class CameraAttacher : MonoBehaviour
+    {
+        #region Private Fields
+
+        [Header("Configurations")]
+        [SerializeField]
+        private bool preserveScale = true;
+
+        #endregion
+
+        #region Unity Callbacks
+
+        private void Start()
+        {
+            Vector3 scale = transform.localScale;
+            
+            transform.SetParent(CameraUtilities.MainCamera.transform);
+
+            if (preserveScale)
+            {
+                transform.localScale = scale;
+            }
+        }
+
+        #endregion
+    }
+}
