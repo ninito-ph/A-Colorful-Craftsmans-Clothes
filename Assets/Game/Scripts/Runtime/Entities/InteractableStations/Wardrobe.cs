994f0e56eba1dea4ed67e5a1498bf9aee732f2a7
Paulo Oliveira <paulo@ninito.me>
2022-01-18 20:59:52 -0300

    Make Wardrobe class inherit from InteractableStation class

---
+++ Assets/Game/Scripts/Runtime/Entities/InteractableStations/Wardrobe.cs
@@ -0,0 +1,33 @@
+﻿using Game.Runtime.Systems.Clothing.Wardrobe;
+using Ninito.UnityExtended.WindowManager;
+using Ninito.UsualSuspects.Interactable;
+using UnityEngine;
+
+namespace Game.Runtime.Entities.InteractableStations
+{
+    /// <summary>
+    /// A simple class that represents the physical wardrobe. 
+    /// </summary>
+    public sealed class Wardrobe : InteractableStation
+    {
+        #region Private Fields
+
+        [Header("Dependencies")]
+        [SerializeField]
+        private WardrobeManager wardrobeManager;
+
+        #endregion
+        
+        #region Private Fields
+
+        public override string InteractionToolTip => "Press E to open the wardrobe";
+        
+        public override void InteractWithAs(IInteractor interactor)
+        {
+            wardrobeManager.ReceivePlayer(interactor.GameObject);
+            base.InteractWithAs(interactor);
+        }
+
+        #endregion
+    }
+}
