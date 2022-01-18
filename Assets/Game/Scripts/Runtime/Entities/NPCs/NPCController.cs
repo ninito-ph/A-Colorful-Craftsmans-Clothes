163eb98b58d9e87d80eae4d7de4465f0d1a26fa6
Paulo Oliveira <paulo@ninito.me>
2022-01-18 18:21:42 -0300

    Add base NPCController abstract class

---
+++ Assets/Game/Scripts/Runtime/Entities/NPCs/NPCController.cs
@@ -0,0 +1,32 @@
+﻿using Game.Runtime.Systems.Dialogue;
+using Ninito.UsualSuspects.Interactable;
+using UnityEngine;
+
+namespace Game.Runtime.Entities
+{
+    /// <summary>
+    /// A base class for all Non Player Characters to inherit from.
+    /// </summary>
+    public abstract class NPCController : MonoBehaviour, IInteractable
+    {
+        #region Private Fields
+
+        [Header("Configuration")]
+        [SerializeField]
+        protected string npcName;
+
+        [Header("Dependencies")]
+        [SerializeField]
+        protected DialogueManager dialogueManager;
+        
+        #endregion
+        
+        #region IInteractable Implementation
+
+        public string InteractionToolTip => "Press E to talk to " + npcName;
+
+        public abstract void InteractWithAs(IInteractor interactor);
+
+        #endregion
+    }
+}
