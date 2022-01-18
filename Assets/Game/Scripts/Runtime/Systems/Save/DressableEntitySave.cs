973084a140a3808ef6e08664f33d723f89db77d1
Paulo Oliveira <paulo@ninito.me>
2022-01-18 10:39:01 -0300

    Add DressableEntitySave class

---
+++ Assets/Game/Scripts/Runtime/Systems/Save/DressableEntitySave.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Runtime.Systems.Save
+{
+    /// <summary>
+    /// A class that holds permanent data regarding the DressableEntity component.
+    /// </summary>
+    [Serializable]
+    public sealed class DressableEntitySave
+    {
+        #region Public Fields
+
+        [SerializeField]
+        public string AccessorySlotItem;
+
+        [SerializeField]
+        public string BodySlotItem;
+
+        #endregion
+
+        #region Constructors
+
+        public DressableEntitySave(string accessorySlotItem, string bodySlotItem)
+        {
+            AccessorySlotItem = accessorySlotItem;
+            BodySlotItem = bodySlotItem;
+        }
+
+        #endregion
+    }
+}
