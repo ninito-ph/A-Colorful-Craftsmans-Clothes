3c8f8c65550cac49f393c8a8a67be5ada2761cf4
Paulo Oliveira <paulo@ninito.me>
2022-01-18 10:40:16 -0300

    Add base InventoryProvider for reduced coupling on inventory interaction

---
+++ Assets/Game/Scripts/Runtime/Systems/Inventory/InventoryProvider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Game.Runtime.Systems.Inventory
+{
+    /// <summary>
+    /// A simple base class for all other classes that provide an inventory
+    /// </summary>
+    public abstract class InventoryProvider : MonoBehaviour
+    {
+        #region Properties
+
+        public abstract ItemInventory Contents { get; }
+
+        #endregion
+    }
+}
