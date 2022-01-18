cdc47cf9bfe93571acada90c9f223c62e71387a7
Paulo Oliveira <paulo@ninito.me>
2022-01-18 20:59:04 -0300

    Add InteractableStation base class

---
+++ Assets/Game/Scripts/Runtime/Entities/InteractableStations/InteractableStation.cs
@@ -0,0 +1,47 @@
+﻿using Ninito.UnityExtended.WindowManager;
+using Ninito.UsualSuspects.Interactable;
+using UnityEngine;
+
+namespace Game.Runtime.Entities.InteractableStations
+{
+    /// <summary>
+    /// A base class for all other interactable station types
+    /// </summary>
+    [RequireComponent(typeof(AudioSource))]
+    public abstract class InteractableStation : MonoBehaviour, IInteractable
+    {
+        #region Protected Fields
+
+        [Header("Dependencies")]
+        [SerializeField]
+        private WindowManager _windowManager;
+
+        [SerializeField]
+        private string windowKey;
+
+        private AudioSource _audioSource;
+        
+        #endregion
+
+        #region Unity Callbacks
+
+        private void Awake()
+        {
+            TryGetComponent(out _audioSource);
+        }
+
+        #endregion
+
+        #region IInteractable Implementation
+
+        public abstract string InteractionToolTip { get; }
+
+        public virtual void InteractWithAs(IInteractor interactor)
+        {
+            _audioSource.Play();
+            _windowManager.SwitchToMenu(windowKey);
+        }
+
+        #endregion
+    }
+}
