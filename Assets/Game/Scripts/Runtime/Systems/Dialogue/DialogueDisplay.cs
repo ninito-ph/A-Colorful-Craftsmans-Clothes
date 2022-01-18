7473abee34eb1b79d670b7d4529a27c89e6740b5
Paulo Oliveira <paulo@ninito.me>
2022-01-18 18:19:30 -0300

    Add DialogueDisplay class to display a DialoguePassage

---
+++ Assets/Game/Scripts/Runtime/Systems/Dialogue/DialogueDisplay.cs
@@ -0,0 +1,36 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Game.Runtime.Systems.Dialogue
+{
+    /// <summary>
+    /// A simple class that controls a dialogue display
+    /// </summary>
+    public sealed class DialogueDisplay : MonoBehaviour
+    {
+        #region Private Fields
+
+        [Header("Dependencies")]
+        [SerializeField]
+        private TMP_Text speakerName;
+        [SerializeField]
+        private TMP_Text bodyText;
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Sets the display's contents
+        /// </summary>
+        /// <param name="speaker">The name of the speaker</param>
+        /// <param name="body">What the speaker is saying</param>
+        public void SetDisplayContents(string speaker, string body)
+        {
+            speakerName.text = speaker;
+            bodyText.text = body;
+        }
+
+        #endregion
+    }
+}
