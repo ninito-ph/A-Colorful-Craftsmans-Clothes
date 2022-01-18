4b7a6c9fb5d5a8d5bb8ea2762c5c45bfa0967513
Paulo Oliveira <paulo@ninito.me>
2022-01-18 18:37:53 -0300

    Add OrderReceiver class to receive order compleiton candidates

---
+++ Assets/Game/Scripts/Runtime/Systems/Orders/OrderReceiver.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Game.Runtime.Data.Attributes;
+using Game.Runtime.Systems.Inventory;
+using UnityEngine;
+
+namespace Game.Runtime.Systems.Orders
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class OrderReceiver : MonoBehaviour
+    {
+        #region Private Fields
+
+        [Header("Dependencies")]
+        [SerializeField]
+        private OrderManager orderManager;
+
+        #endregion
+
+        #region Public Methods
+
+        public int TryCompleteOrders(InventoryProvider inventoryProvider)
+        {
+            List<Order> activeOrders = orderManager.Orders;
+            int completedOrders = 0;
+            int currentlyActiveOrders = activeOrders.Count;
+            
+            for (int index = currentlyActiveOrders - 1; index >= 0; index--)
+            {
+                ClothingAttributes adequateItem = GetMatchingClothingAttributes(inventoryProvider, activeOrders[index]);
+
+                if (!inventoryProvider.Contents.TryRemoveItem(adequateItem)) continue;
+                completedOrders++;
+                activeOrders[index].IsCompleted = true;
+            }
+
+            Debug.Log(completedOrders);
+            return completedOrders;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Gets an item that matches the order's demand
+        /// </summary>
+        /// <param name="inventoryProvider">The inventory provider to check</param>
+        /// <param name="order">The order to compare to</param>
+        /// <returns>The matching clothing item</returns>
+        private static ClothingAttributes GetMatchingClothingAttributes(InventoryProvider inventoryProvider,
+            Order order)
+        {
+            return inventoryProvider.Contents.ItemsByQuantity.Keys
+                .Where(item => item as ClothingAttributes != null)
+                .Cast<ClothingAttributes>().FirstOrDefault(order.MeetsOrderDemand);
+        }
+
+        #endregion
+    }
+}
