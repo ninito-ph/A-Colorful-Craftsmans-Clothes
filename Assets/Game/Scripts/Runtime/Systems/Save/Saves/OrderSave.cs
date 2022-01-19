70a3aa5581af395f06d860e7c4bd9a83f8e7ea6c
Paulo Oliveira <paulo@ninito.me>
2022-01-19 00:32:31 -0300

    Add OrderSave class

---
+++ Assets/Game/Scripts/Runtime/Systems/Save/Saves/OrderSave.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Game.Runtime.Systems.Orders;
+using UnityEngine;
+
+namespace Game.Runtime.Systems.Save.Saves
+{
+    /// <summary>
+    /// A class that contains permanent data for a single order <see cref="Order"/>
+    /// </summary>
+    [Serializable]
+    public sealed class OrderSave
+    {
+        #region Public Fields
+
+        [SerializeField]
+        public string StoredItem;
+        [SerializeField]
+        public float RemainingTime;
+
+        [SerializeField]
+        public float ColorTolerance;
+
+        #endregion
+
+        #region Constructors
+
+        public OrderSave(string storedItem, float remainingTime, float colorTolerance)
+        {
+            StoredItem = storedItem;
+            RemainingTime = remainingTime;
+            ColorTolerance = colorTolerance;
+        }
+
+        #endregion
+    }
+}
