4cf13576098a923dfabafec7b1433da7df2915c9
Paulo Oliveira <paulo@ninito.me>
2022-01-19 16:28:05 -0300

    Add SimpleSpeakingEntity script for interactable objects with text

---
+++ Assets/Game/Scripts/Runtime/Entities/SimpleSpeakingEntity.cs
@@ -0,0 +1,64 @@
+﻿using Game.Runtime.Systems.Dialogue;
+using Ninito.UsualSuspects.Interactable;
+using UnityEngine;
+
+namespace Game.Runtime.Entities
+{
+    /// <summary>
+    /// A class that represents an entity with simple dialogue
+    /// </summary>
+    public sealed class SimpleSpeakingEntity : MonoBehaviour, IInteractable
+    {
+        #region Private Fields
+
+        [Header("Configurations")]
+        [SerializeField]
+        [TextArea]
+        private string dialogue;
+
+        [SerializeField]
+        private string speakerName;
+
+        [SerializeField]
+        private string interactionTooltip;
+
+        private DialogueManager _dialogueManager;
+        private DialoguePassage[] _dialoguePassage;
+
+        #endregion
+
+        #region Unity Callbacks
+
+        private void Awake()
+        {
+            GenerateDialogue();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Generates dialogue based on the simple text in the dialogue field.
+        /// </summary>
+        private void GenerateDialogue()
+        {
+            _dialoguePassage = new[] { ScriptableObject.CreateInstance<DialoguePassage>() };
+            _dialoguePassage[0].InternalText = dialogue;
+            _dialoguePassage[0].SpeakerName = speakerName;
+        }
+
+        #endregion
+
+        #region IInteractable Implementation
+
+        public string InteractionToolTip => interactionTooltip;
+
+        public void InteractWithAs(IInteractor interactor)
+        {
+            _dialogueManager.StartDialogue(_dialoguePassage, transform);
+        }
+
+        #endregion
+    }
+}
