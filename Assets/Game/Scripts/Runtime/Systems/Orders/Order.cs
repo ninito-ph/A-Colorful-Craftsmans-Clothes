7c52dc740ec91f29d11e1211b28ca1e553db2d94
Paulo Oliveira <paulo@ninito.me>
2022-01-19 00:34:22 -0300

    Add Store and Restore method to Order class

--- Assets/Game/Scripts/Runtime/Systems/Orders/Order.cs
+++ Assets/Game/Scripts/Runtime/Systems/Orders/Order.cs
@@ -1,4 +1,8 @@
-﻿using Game.Runtime.Data.Attributes;
+﻿using System;
+using Game.Runtime.Core;
+using Game.Runtime.Data.Attributes;
+using Game.Runtime.Data.Registries;
+using Game.Runtime.Systems.Save.Saves;
 using Game.Runtime.Utility;
 using UnityEngine;
 
@@ -16,9 +20,11 @@ namespace Game.Runtime.Systems.Orders
         public float ColorTolerance { get; set; }
 
         public float EndTime { get; set; }
-        public float RemainingTime => EndTime - Time.time;
         
-        public bool IsExpired => Time.time >= EndTime;
+        // Look at the Store method for why I'm using LastSavedTime instead of Time.time
+        public float RemainingTime => EndTime - GameManager.Instance.LastSavedTime;
+
+        public bool IsExpired => GameManager.Instance.LastSavedTime >= EndTime;
         public bool IsCompleted { get; set; } = false;
 
         #endregion
@@ -57,9 +63,44 @@ namespace Game.Runtime.Systems.Orders
         public bool MeetsOrderDemand(ClothingAttributes clothing)
         {
             if (clothing == null) return false;
-            
+
             return clothing.ID == Item.ID &&
                    RGBandCMYKUtility.AreColorsSimilar(clothing.Color, Item.Color, ColorTolerance);
+        }
+
+        /// <summary>
+        /// Stores the order in a serialized JSON string
+        /// </summary>
+        /// <returns>The serialized JSON string of the order</returns>
+        public string Store()
+        {
+            /*
+             * You have got to be fucking kidding me. Time.time returns 0 when called at the frame where the game is
+             * closed, and the best part is that its NOWHERE to be found in the Unity documentation. This is beyond
+             * human comprehension. I spent hours figuring out why RemainingTime was not accurate here.
+             */
+            OrderSave save = new OrderSave(Item.Store(), RemainingTime, ColorTolerance);
+            return JsonUtility.ToJson(save);
+        }
+
+        /// <summary>
+        /// Restores the Order from its serialized JSON form
+        /// </summary>
+        /// <param name="data">The data to restore</param>
+        /// <param name="registry">The registry to restore with</param>
+        /// <returns>The restored Order object</returns>
+        public static Order Restore(string data, ItemRegistry registry)
+        {
+            if (String.IsNullOrEmpty(data)) return null;
+
+            OrderSave save = JsonUtility.FromJson<OrderSave>(data);
+
+            Order order = CreateNew(ItemAttributes.Restore(save.StoredItem, registry) as ClothingAttributes,
+                save.RemainingTime,
+                save.ColorTolerance);
+            order.EndTime = Time.time + save.RemainingTime;
+
+            return order;
         }
 
         #endregion
