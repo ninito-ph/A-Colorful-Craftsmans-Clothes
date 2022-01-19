74cb8c753f38a7892ad830f2fb8d746a79ab61c0
Paulo Oliveira <paulo@ninito.me>
2022-01-19 20:35:17 -0300

    Add Door class to allow player to change scenes

---
+++ Assets/Game/Scripts/Runtime/Entities/Door.cs
@@ -0,0 +1,34 @@
+﻿using Game.Runtime.Core;
+using Ninito.UsualSuspects.Interactable;
+using UnityEngine;
+
+namespace Game.Runtime.Entities
+{
+    /// <summary>
+    /// A diegetic object which allows the player to change scenes
+    /// </summary>
+    public sealed class Door : MonoBehaviour, IInteractable
+    {
+        #region Public Fields
+
+        [Header("Configurations")]
+        [SerializeField]
+        private string sceneName;
+
+        [SerializeField]
+        private string interactionText;
+        
+        #endregion
+        
+        #region IInteractable Implementation
+
+        public string InteractionToolTip => interactionText;
+        
+        public void InteractWithAs(IInteractor interactor)
+        {
+            GameManager.Instance.LoadScene(sceneName);
+        }
+
+        #endregion
+    }
+}
