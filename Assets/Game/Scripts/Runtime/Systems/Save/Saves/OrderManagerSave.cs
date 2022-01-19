8383079605c4919c2eb0fd53a6aed10561648cbe
Paulo Oliveira <paulo@ninito.me>
2022-01-19 00:36:33 -0300

    Add OrderManagerSave class

---
+++ Assets/Game/Scripts/Runtime/Systems/Save/Saves/OrderManagerSave.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Game.Runtime.Systems.Orders;
+using UnityEngine;
+
+namespace Game.Runtime.Systems.Save.Saves
+{
+    /// <summary>
+    /// A class that contains permanent data for the <see cref="OrderManager"/>
+    /// </summary>
+    [Serializable]
+    public sealed class OrderManagerSave
+    {
+        #region Public Fields
+
+        [SerializeField]
+        public List<string> Orders;
+
+        #endregion
+
+        #region Constructors
+
+        public OrderManagerSave(List<string> orders)
+        {
+            Orders = orders;
+        }
+
+        #endregion
+    }
+}
