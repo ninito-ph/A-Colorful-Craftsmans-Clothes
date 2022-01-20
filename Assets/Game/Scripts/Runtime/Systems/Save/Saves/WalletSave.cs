4e8b17a6ea3190480446f7e43db8aa80f04eb3b6
Paulo Oliveira <paulo@ninito.me>
2022-01-20 02:54:16 -0300

    Add simple WalletSave class

---
+++ Assets/Game/Scripts/Runtime/Systems/Save/Saves/WalletSave.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Runtime.Systems.Save.Saves
+{
+    /// <summary>
+    /// A simple class that contains permanent data for the wallet
+    /// </summary>
+    [Serializable]
+    public class WalletSave
+    {
+        #region Public Fields
+
+        [SerializeField]
+        public float balance;
+
+        #endregion
+
+        #region Constructors
+
+        public WalletSave(float saveBalance)
+        {
+            balance = saveBalance;
+        }
+
+        #endregion
+    }
+}
