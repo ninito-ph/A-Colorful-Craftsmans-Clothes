e5b3172c628f163c0dae7610efd5f0c551741c3a
Paulo Oliveira <paulo@ninito.me>
2022-01-20 02:53:37 -0300

    Add simple wallet class to keep an amount of currency

---
+++ Assets/Game/Scripts/Runtime/Systems/Wallet/Wallet.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Game.Runtime.Systems.Save;
+using Game.Runtime.Systems.Save.Saves;
+using UnityEngine;
+
+namespace Game.Runtime.Systems.Wallet
+{
+    /// <summary>
+    /// A simple class that controls the player's wallet
+    /// </summary>
+    public sealed class Wallet : MonoBehaviour
+    {
+        #region Private Fields
+
+        private float _balance = 0;
+
+        #endregion
+
+        #region Properties
+
+        public float Balance
+        {
+            get => _balance;
+            set
+            {
+                _balance = value;
+                WalletUpdated?.Invoke();
+            }
+        }
+
+        public Action WalletUpdated { get; set; }
+
+        private string SaveKey => gameObject + ".wallet";
+
+        #endregion
+
+        #region UnityCallbacks
+
+        private void Start()
+        {
+            if (DataLoader.TryLoad(SaveKey, out WalletSave save))
+            {
+                Balance = save.balance;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            WalletSave save = new WalletSave(Balance);
+            DataSaver.SaveData(save, SaveKey);
+        }
+
+        #endregion
+    }
+}
