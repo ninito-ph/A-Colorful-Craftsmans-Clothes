372f97e26434fb8ec7890871f5af032f8b9bc27b
Paulo Oliveira <paulo@ninito.me>
2022-01-20 03:00:23 -0300

    Add HarvestableProvider class to provide random HarvestableAttributes

---
+++ Assets/Game/Scripts/Runtime/Entities/Harvestables/HarvestableProvider.cs
@@ -0,0 +1,34 @@
+﻿using Game.Runtime.Data.Attributes;
+using UnityEngine;
+
+namespace Game.Runtime.Entities.Harvestables
+{
+    /// <summary>
+    /// A class that provides a HarvestableAttributes randomly
+    /// </summary>
+    [CreateAssetMenu(fileName = CreateMenus.HARVESTABLE_PROVIDER_FILENAME,
+        menuName = CreateMenus.HARVESTABLE_PROVIDER_MENUNAME, order = CreateMenus.HARVESTABLE_PROVIDER_ORDER)]
+    public sealed class HarvestableProvider : ScriptableObject
+    {
+        #region Private Fields
+
+        [Header("Configurations")]
+        [SerializeField]
+        private HarvestableAttributes[] existingHarvestables;
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Get a random HarvestableAttributes
+        /// </summary>
+        /// <returns>A random HarvestableAttributes</returns>
+        public HarvestableAttributes GetHarvestable()
+        {
+            return existingHarvestables[Random.Range(0, existingHarvestables.Length)];
+        }
+
+        #endregion
+    }
+}
