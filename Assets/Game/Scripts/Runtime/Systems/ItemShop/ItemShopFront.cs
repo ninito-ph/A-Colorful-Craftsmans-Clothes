b52454fa003bd693086adad0bd1bf0f488534bfd
Paulo Oliveira <paulo@ninito.me>
2022-01-20 05:31:00 -0300

    Add ItemShopFront for a physical representation of a shop

---
+++ Assets/Game/Scripts/Runtime/Systems/ItemShop/ItemShopFront.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Game.Runtime.Systems.Inventory;
+using Ninito.UnityExtended.WindowManager;
+using Ninito.UsualSuspects.Interactable;
+using UnityEngine;
+
+namespace Game.Runtime.Systems.ItemShop
+{
+    /// <summary>
+    /// A simple class that opens a window to buy items.
+    /// </summary>
+    public sealed class ItemShopFront : MonoBehaviour, IInteractable
+    {
+        #region Private Fields
+
+        [Header("Dependencies")]
+        [SerializeField]
+        private WindowManager windowManager;
+
+        [SerializeField]
+        private string shopMenuKey;
+
+        [SerializeField]
+        private string interactionText = "Press E to open the Dye shop";
+
+        [SerializeField]
+        private ItemShop itemShop;
+
+        #endregion
+
+        #region Properties
+
+        public string InteractionToolTip => interactionText;
+
+        public void InteractWithAs(IInteractor interactor)
+        {
+            itemShop.BeginSale(interactor.GameObject.GetComponent<Wallet.Wallet>(),
+                interactor.GameObject.GetComponent<InventoryProvider>());
+            windowManager.SwitchToMenu(shopMenuKey);
+        }
+
+        #endregion
+    }
+}
