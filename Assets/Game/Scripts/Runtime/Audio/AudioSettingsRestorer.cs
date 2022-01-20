6c0b14cb64b559aa91ac9161c53374fbae42e1a6
Paulo Oliveira <paulo@ninito.me>
2022-01-20 07:01:06 -0300

    Add AudioSettingRestorer class to load volume on Bootstrap

---
+++ Assets/Game/Scripts/Runtime/Audio/AudioSettingsRestorer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Game.Runtime.Audio
+{
+    /// <summary>
+    /// A class that restores audio settings at startup
+    /// </summary>
+    public sealed class AudioSettingsRestorer : MonoBehaviour
+    {
+        #region Private Fields
+
+        [Header("Dependencies")]
+        [SerializeField]
+        private AudioMixer audioMixer;
+
+        [SerializeField]
+        private string[] audioGroupNames;
+
+        #endregion
+
+        #region Unity Callbacks
+
+        private void Awake()
+        {
+            foreach (string groupName in audioGroupNames)
+            {
+                float volume = PlayerPrefs.GetFloat(groupName + "Volume", 1f);
+                audioMixer.SetFloat(groupName, Mathf.Log10(volume) * 20);
+            }
+        }
+
+        #endregion
+    }
+}
