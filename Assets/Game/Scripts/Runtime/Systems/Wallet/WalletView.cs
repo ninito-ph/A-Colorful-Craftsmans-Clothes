2da7776dbe1e4d69f34c142fdb0f229d6801971b
Paulo Oliveira <paulo@ninito.me>
2022-01-20 02:53:55 -0300

    Add simple WalletView class to visualize wallet contents

---
+++ Assets/Game/Scripts/Runtime/Systems/Wallet/WalletView.cs
@@ -0,0 +1,52 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Game.Runtime.Systems.Wallet
+{
+    /// <summary>
+    /// A simple class that displays a wallet's contents
+    /// </summary>
+    public sealed class WalletView : MonoBehaviour
+    {
+        #region Private Fields
+
+        [Header("Dependencies")]
+        [SerializeField]
+        private Wallet wallet;
+
+        [SerializeField]
+        private TMP_Text balanceText;
+
+        [SerializeField]
+        private string appendToCurrency = "$";
+        
+        #endregion
+
+        #region Unity Callbacks
+
+        private void OnEnable()
+        {
+            wallet.WalletUpdated += UpdateBalanceDisplay;
+            UpdateBalanceDisplay();
+        }
+
+        private void OnDisable()
+        {
+            wallet.WalletUpdated -= UpdateBalanceDisplay;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Updates the wallet's display with the current amount
+        /// </summary>
+        private void UpdateBalanceDisplay()
+        {
+            balanceText.text = Mathf.RoundToInt(wallet.Balance) + appendToCurrency;
+        }
+
+        #endregion
+    }
+}
