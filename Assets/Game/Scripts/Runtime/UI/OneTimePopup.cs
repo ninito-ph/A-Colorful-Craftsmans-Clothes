83ddd0355851b410fc5f5b0d6c53b72a9b2f6c61
Paulo Oliveira <paulo@ninito.me>
2022-01-20 05:31:35 -0300

    Add OneTimePopup class

---
+++ Assets/Game/Scripts/Runtime/UI/OneTimePopup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Game.Runtime.Core;
+using UnityEngine;
+
+namespace Game.Runtime.UI
+{
+    /// <summary>
+    /// A class that shows a pop up that should only appear once
+    /// </summary>
+    public sealed class OneTimePopup : MonoBehaviour
+    {
+        #region Private Fields
+
+        [Header("Configurations")]
+        [SerializeField]
+        private string popUpKey = "";
+
+        #endregion
+
+        #region Unity Callbacks
+
+        private void Start()
+        {
+            GameManager.Instance.SetPlayerInputEnabled(false);
+            
+            if (PlayerPrefs.GetInt(popUpKey, 0) == 1)
+            {
+                Destroy(gameObject);
+            }
+            
+            PlayerPrefs.SetInt(popUpKey, 1);
+        }
+
+        private void OnDestroy()
+        {
+            GameManager.Instance.SetPlayerInputEnabled(true);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Close()
+        {
+            Destroy(gameObject);
+        }
+
+        #endregion
+    }
+}
